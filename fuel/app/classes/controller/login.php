0eb1b95f59c9ad25c9eca3f72f4ff9e4d125fe9e
daisuke-senmyou <daisuke_senmyou@cybird.co.jp>
2015-11-10 16:10:55 +0900

    デバッグ文削除

--- fuel/app/classes/controller/login.php
+++ fuel/app/classes/controller/login.php
@@ -4,18 +4,14 @@ use \Model\User;
 class Controller_Login extends Controller
 {
 	public function action_index() {
-		//$data['user'] = User::get_results();
-		//return Response::forge(View::forge('hello',$data));
 		return View::forge('login/form');
 	}
 
 	public function action_submit() {
 		$userid = Input::post('id');
 		$password = Input::post('password');
-Log::debug("userid = " . $userid);
 
 		$result = User::check_login($userid, $password);
-Log::debug("result = " . $result);
 
 		if($result) {
 			return View::forge('login/success');
