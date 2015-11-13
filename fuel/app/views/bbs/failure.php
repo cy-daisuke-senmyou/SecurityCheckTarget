0f16b4e7ea658f8236193d3bf304c3324dc5acf0
daisuke-senmyou <daisuke_senmyou@cybird.co.jp>
2015-11-13 19:45:47 +0900

    文言修正

--- fuel/app/views/bbs/failure.php
+++ fuel/app/views/bbs/failure.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>LoginForm</title>
+	<title>BBS</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php echo Asset::css('bootstrap.css'); ?>
 	<?php echo Asset::js('bootstrap.js'); ?>
