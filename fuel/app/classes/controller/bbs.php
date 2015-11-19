0c4e7bcccf2e97789687ef9f6d71a3044ab053aa
daisuke-senmyou <daisuke_senmyou@cybird.co.jp>
2015-11-19 19:18:00 +0900

    本来のFuel的な記述に戻す。

--- fuel/app/classes/controller/bbs.php
+++ fuel/app/classes/controller/bbs.php
@@ -5,19 +5,21 @@ use \Model\Bbs;
 class Controller_Bbs extends Controller
 {
 	public function action_index() {
-		$bbs = new Bbs();
-		$data['all_post'] = $bbs->get_all_post();
+		//$bbs = new Bbs();
+		//$data['all_post'] = $bbs->get_all_post();
+		$data['all_post'] = Bbs::get_all_post();
 		$view = View::forge('bbs/index');
 		$view->set('all_post', $data['all_post'], false);
 		return $view;
 	}
 
 	public function action_submit() {
-		$bbs = new Bbs();
 		$name = Input::post('name');
 		//$message = Input::post('message');
 		$message = $_POST['message'];
-		$result = $bbs->post($name, $message);
+		//$bbs = new Bbs();
+		//$result = $bbs->post($name, $message);
+		$result = Bbs::post($name, $message);
 
 		if($result) {
 			return Response::redirect('bbs/index');;
